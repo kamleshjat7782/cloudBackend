ee13ba5fe53853b8ddf95bc8d50a77652fa357a3
kamlesh choudhary <kamleshjat7782@gmail.com>
2024-06-03 13:42:53 +0530

    update server file

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/api/v1/cart', cartRouters);
 
 //rest api
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+  res.send("<h1>Welcome to Cloud App </h1>");
 });
 
 //PORT
